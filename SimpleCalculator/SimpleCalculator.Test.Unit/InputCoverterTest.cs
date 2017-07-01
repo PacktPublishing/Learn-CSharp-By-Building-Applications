5f8f7aecbb63321cee998b39fc0778c1c7d2b08a
Avetis Ghukasyan <avetis.ghukasyan@outlook.com>
2017-07-01 18:05:38 -0400

    Initial app commit.

---
+++ SimpleCalculator/SimpleCalculator.Test.Unit/InputCoverterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class InputCoverterTest
+    {
+        private readonly InputConverter _inputConverter = new InputConverter();
+
+        [TestMethod]
+        public void ConvertsValidStringInputIntoDouble()
+        {
+            string inputNumber = "5";
+            double convertedNumber = _inputConverter.ConvertInputToNumberic(inputNumber);
+            Assert.AreEqual(convertedNumber, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Expected a numeric value.")]
+        public void FailsToConvertInvalidStringInputIntoDouble()
+        {
+            string inputNumber = "*";
+            double convertedNumber = _inputConverter.ConvertInputToNumberic(inputNumber);
+        }
+    }
+}
