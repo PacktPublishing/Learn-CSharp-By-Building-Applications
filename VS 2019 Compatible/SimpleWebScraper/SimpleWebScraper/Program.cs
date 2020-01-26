d4c7006d4d42ee2c6146fccdd1d2ecefffa93244
AvetisG <avetis.ghukasyan@outlook.com>
2020-01-25 19:36:05 -0500

    Checking in the new 2019 update so that the code and tests can run properly on Visual Studio 2019.

---
+++ VS 2019 Compatible/SimpleWebScraper/SimpleWebScraper/Program.cs
@@ -0,0 +1,66 @@
+﻿using SimpleWebScraper.Builders;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleWebScraper
+{
+    class Program
+    {
+        private const string Method = "search";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter which city you would like to scrape information from:");
+                var craigslistCity = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine("Please enter the CraigsList category that you would like to scrape:");
+                var craigslistCategoryName = Console.ReadLine() ?? string.Empty;
+
+                using (WebClient client = new WebClient())
+                {
+                    string content = client.DownloadString($"http://{craigslistCity.Replace(" ", string.Empty)}.craigslist.org/{Method}/{craigslistCategoryName}");
+
+                    ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .WithData(content)
+                        .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.ExplicitCapture)
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@"href=\""(.*?)\""")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+                    Scraper scraper = new Scraper();
+
+                    var scrapedElements = scraper.Scrape(scrapeCriteria);
+
+                    if (scrapedElements.Any())
+                    {
+                        foreach (var scrapedElement in scrapedElements) Console.WriteLine(scrapedElement);
+                    }
+                    else
+                    {
+                        Console.WriteLine("There were no matches for the specified scrape criteria.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
